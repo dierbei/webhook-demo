f9ee6cabf0c2ec82a012fa4dbe244019966036e1
xiaolatiao <1628652790@qq.com>
2022-03-07 22:46:22 +0800

    fix:修改服务路由

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/pods", func(w http.ResponseWriter, r *http.Request) {
 		log.Println(r.RequestURI)
 		var body []byte
 		if r.Body != nil {
